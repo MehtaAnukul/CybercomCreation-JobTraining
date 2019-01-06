5da11f84bd16822aaaf72fcfcfdac1ea4fe7a886
anukul mehta <mehtaanukul@gmail.com>
2019-01-06 14:10:46 +0530

    implicit

---
+++ ExplicitAndImplicitAppB/app/src/main/java/com/anukul/explicitandimplicitappb/MainActivity.java
@@ -0,0 +1,29 @@
+package com.anukul.explicitandimplicitappb;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+        private Button sendBtn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sendBtn = findViewById(R.id.activity_main_sendBtn);
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra("Message","This is Simple message");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ ExplicitAndImplicitDemo/app/src/main/java/com/anukul/explicitandimplicitdemo/MainActivity.java
@@ -0,0 +1,49 @@
+package com.anukul.explicitandimplicitdemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button shareBtn;
+    private Button sendBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        shareBtn = findViewById(R.id.activity_main_sharePostBtn);
+        sendBtn = findViewById(R.id.activity_main_sendMailBtn);
+
+        shareBtn.setOnClickListener(this);
+        sendBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activity_main_sharePostBtn:
+                sharePost();
+                break;
+            case R.id.activity_main_sendMailBtn:
+                sendMail();
+                break;
+        }
+    }
+
+    private void sendMail() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra("Message","This is a simple message");
+        startActivity(intent);
+    }
+
+    private void sharePost() {
+        Intent intent = new Intent(this,SharePostActivity.class);
+        startActivity(intent);
+    }
+}
