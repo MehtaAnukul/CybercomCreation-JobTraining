6a56d73c102fdd918c5a5f535914c5c8049104e2
anukul mehta <mehtaanukul@gmail.com>
2019-01-28 14:29:05 +0530

    new update

--- NotificationProgressbarDemo/app/src/main/java/com/anukul/notificationprogressbardemo/MainActivity.java
+++ NotificationProgressbarDemo/app/src/main/java/com/anukul/notificationprogressbardemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.anukul.notificationprogressbardemo;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
@@ -13,15 +15,20 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Button notificationProgressBtn;
+    private Button notificationImgBtn;
+    private Button notificationLongTextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         notificationProgressBtn = findViewById(R.id.activitiy_main_notificationProgressBtn);
+        notificationImgBtn = findViewById(R.id.activity_main_notificationImgBtn);
+        notificationLongTextBtn = findViewById(R.id.activity_main_notificationLongTextBtn);
 
         notificationProgressBtn.setOnClickListener(this);
-
+        notificationImgBtn.setOnClickListener(this);
+        notificationLongTextBtn.setOnClickListener(this);
 
     }
 
@@ -31,7 +38,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.activitiy_main_notificationProgressBtn:
                 openNotificationProgress();
                 break;
+            case R.id.activity_main_notificationImgBtn:
+                openNotificationImg();
+                break;
+            case R.id.activity_main_notificationLongTextBtn:
+                openNotificationLongText();
+                break;
         }
+    }
+
+    private void openNotificationLongText() {
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,AppConstant.KAY_CHANNET_ID);
+        builder.setSmallIcon(R.drawable.ic_download);
+        builder.setContentTitle("LongText Notification");
+        builder.setContentText("This is long Text");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(getString(R.string.notificationtext)));
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(AppConstant.KAY_NOTIFICATION_ID,builder.build());
+
+    }
+
+    private void openNotificationImg() {
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this,AppConstant.KAY_CHANNET_ID);
+        builder.setSmallIcon(R.drawable.ic_download);
+        builder.setContentTitle("Image Notification");
+        builder.setContentText("Qutar Airways Img");
+        builder.setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.qatar_aieways);
+        builder.setLargeIcon(bitmap);
+        builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap).bigLargeIcon(null));
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(AppConstant.KAY_NOTIFICATION_ID,builder.build());
+
+
     }
 
     private void openNotificationProgress() {
