8019556417b3dce5f0b6019fe85f007c51fc29bf
anukul mehta <mehtaanukul@gmail.com>
2019-02-01 13:20:38 +0530

    alert

--- AlertUsingDialogFragment/app/src/main/java/com/anukul/alertusingdialogfragment/MainActivity.java
+++ AlertUsingDialogFragment/app/src/main/java/com/anukul/alertusingdialogfragment/MainActivity.java
@@ -5,23 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-    private Button alertBtn;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button fireMissileAlertBtn;
+    private Button colorAlertBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        alertBtn = findViewById(R.id.activity_main_alertBtn);
+        fireMissileAlertBtn = findViewById(R.id.activity_main_fireMissileAlertBtn);
+        colorAlertBtn = findViewById(R.id.activity_main_colorAlertBtn);
 
-        alertBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        fireMissileAlertBtn.setOnClickListener(this);
+        colorAlertBtn.setOnClickListener(this);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.activity_main_fireMissileAlertBtn:
                 new FireMissileDialog().show(getSupportFragmentManager(), "missile");
-
-            }
-        });
+                break;
+            case R.id.activity_main_colorAlertBtn:
+                new MyDialog().show(getSupportFragmentManager(),"Color");
+                break;
+        }
     }
 }
