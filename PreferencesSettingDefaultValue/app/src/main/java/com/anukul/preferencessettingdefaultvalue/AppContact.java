165b6b8385d07974e2645c888862a9d38474ff7a
anukul mehta <mehtaanukul@gmail.com>
2019-02-23 19:57:34 +0530

    update

--- PreferencesSettingDefaultValue/app/src/main/java/com/anukul/preferencessettingdefaultvalue/AppContact.java
+++ PreferencesSettingDefaultValue/app/src/main/java/com/anukul/preferencessettingdefaultvalue/AppContact.java
@@ -3,4 +3,13 @@ package com.anukul.preferencessettingdefaultvalue;
 public class AppContact {
     public static final String PREF_SMS_LIMIT = "sms_delete_limit";
     public static final String PREF_MMS_LIMIT = "mms_delete_limit";
+
+    public static final String PREF_DELETE_OLD_MESSAGE = "pref_delete_old_message";
+    public static final String PREF_SMS_DELETE_LIMIT = "sms_delete_limit";
+    public static final String PREF_MMS_DELETE_LIMIT = "mms_delete_limit";
+    public static final String PREF_SMS_DELIVERY_REPORT = "pref_delivery_reports_sms";
+    public static final String PREF_MMS_DELIVERY_REPORT = "pref_delivery_reports_mms";
+
+
+
 }
--- PreferencesSettingDefaultValue/app/src/main/java/com/anukul/preferencessettingdefaultvalue/MainActivity.java
+++ PreferencesSettingDefaultValue/app/src/main/java/com/anukul/preferencessettingdefaultvalue/MainActivity.java
@@ -1,15 +1,26 @@
 package com.anukul.preferencessettingdefaultvalue;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private Toolbar toolbar;
+
+    private TextView msgDeleteStatusTv;
+    private TextView smsDeleteLimitTv;
+    private TextView mmsDeleteLimitTv;
+    private TextView smsDeliveryReportTv;
+    private TextView mmsDeliveryReportTv;
+    private Button readSettingBtn;
 
 
     @Override
@@ -20,7 +31,17 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar_layout_toolbar);
         setSupportActionBar(toolbar);
 
+        msgDeleteStatusTv = findViewById(R.id.activity_main_msgDeleteStatusTv);
+        smsDeleteLimitTv = findViewById(R.id.activity_main_smsDeleteLimitTv);
+        mmsDeleteLimitTv = findViewById(R.id.activity_main_mmsDeleteLimitTv);
+        smsDeliveryReportTv = findViewById(R.id.activity_main_smsDeliveryReportTv);
+        mmsDeliveryReportTv = findViewById(R.id.activity_main_mmsDeliveryReportTv);
+
+        readSettingBtn = findViewById(R.id.activity_main_readSettingBtn);
+        readSettingBtn.setOnClickListener(this);
+
         PreferenceManager.setDefaultValues(this,R.xml.preferences_group_setting,false);
+
 
 
     }
@@ -41,5 +62,26 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.activity_main_readSettingBtn:
+                readSetting();
+                break;
+        }
+    }
+
+
+    private void readSetting() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        msgDeleteStatusTv.setText("Message Delete Stutas : " +String.valueOf(sharedPreferences.getBoolean(AppContact.PREF_DELETE_OLD_MESSAGE,false)));
+        smsDeleteLimitTv.setText("SMS Delete Limit : "+sharedPreferences.getString(AppContact.PREF_SMS_DELETE_LIMIT,"0"));
+        mmsDeleteLimitTv.setText("MMS Delete Limit : "+sharedPreferences.getString(AppContact.PREF_MMS_DELETE_LIMIT,"0"));
+
+        smsDeliveryReportTv.setText("SMS Delivery Report : "+String.valueOf(sharedPreferences.getBoolean(AppContact.PREF_SMS_DELIVERY_REPORT,false)));
+        mmsDeliveryReportTv.setText("MMS Delivery Report : "+String.valueOf(sharedPreferences.getBoolean(AppContact.PREF_MMS_DELIVERY_REPORT,false)));
     }
 }
