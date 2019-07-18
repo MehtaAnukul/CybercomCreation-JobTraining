67374be598cadef508d023a025e055e405473c94
anukul mehta <mehtaanukul@gmail.com>
2019-07-18 14:36:05 +0530

    new update

--- RetrofitExample/app/src/main/java/com/anukul/retrofitexample/ApiInterface.java
+++ RetrofitExample/app/src/main/java/com/anukul/retrofitexample/ApiInterface.java
@@ -2,6 +2,7 @@ package com.anukul.retrofitexample;
 
 import com.anukul.retrofitexample.model.MovieResponse;
 
+
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
@@ -9,6 +10,7 @@ import retrofit2.http.Query;
 
 public interface ApiInterface {
 
+    //http://api.themoviedb.org/3/movie/top_rated?api_key=c17fbafb7a92b964d7a058120682e0b4
     @GET("movie/top_rated")
     Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
 
@@ -16,3 +18,5 @@ public interface ApiInterface {
     Call<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
 
 }
+   // @GET("marvel")
+   // Call<List<MovieModel>> getMovies();
